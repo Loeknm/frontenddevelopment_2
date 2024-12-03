ea559bdf4a94a91097989c4fdaa3564b03ce9617
Loeknm <147065880+Loeknm@users.noreply.github.com>
2024-12-03 14:18:00 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,18 @@
 // JavaScript Document
 console.log("hi");
+
+
+
+
+
+let currentSlide = 0;
+
+function changeSlide(direction) {
+    const slides = document.querySelectorAll('.carousel-slide');
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + direction + slides.length) % slides.length;
+    slides[currentSlide].classList.add('active');
+    document.querySelector('.carousel-container').style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+document.querySelectorAll('.carousel-slide')[currentSlide].classList.add('active');
